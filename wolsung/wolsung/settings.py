777127147ae2a1198b317fab1fac99206621fe6e
Mateusz <33525024+Falien164@users.noreply.github.com>
2022-11-01 20:07:45 +0100

    add swagger

--- wolsung/wolsung/settings.py
+++ wolsung/wolsung/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'characters.apps.CharactersConfig',
+    'rest_framework',
+    'drf_yasg',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- wolsung/wolsung/urls.py
+++ wolsung/wolsung/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Wolsung Character API",
+      default_version='0.0.1',
+      description="Test description",
+      contact=openapi.Contact(url="github.com/Falien164"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path('docs', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('characters/', include('characters.urls')),
     path('admin/', admin.site.urls),
 ]
